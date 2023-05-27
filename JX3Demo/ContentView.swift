478b73a4966e206325cc61d104874fe5cf8da53d
wttch（王冲） <wttch@wttch.com>
2023-05-28 01:43:59 +0800

    Initial Commit

---
+++ JX3Demo/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  JX3Demo
+//
+//  Created by Wttch on 2023/5/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ JX3Demo/JX3DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  JX3DemoApp.swift
+//  JX3Demo
+//
+//  Created by Wttch on 2023/5/28.
+//
+
+import SwiftUI
+
+@main
+struct JX3DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
