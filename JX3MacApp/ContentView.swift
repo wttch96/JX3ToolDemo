84342ed7e7839009b02e3b5b5c1e4c33a8d2a7a3
wttch（王冲） <wttch@wttch.com>
2023-05-29 01:02:19 +0800

    添加 mac app

---
+++ JX3MacApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  JX3MacApp
+//
+//  Created by Wttch on 2023/5/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ JX3MacApp/JX3MacAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JX3MacAppApp.swift
+//  JX3MacApp
+//
+//  Created by Wttch on 2023/5/29.
+//
+
+import SwiftUI
+
+@main
+struct JX3MacAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
